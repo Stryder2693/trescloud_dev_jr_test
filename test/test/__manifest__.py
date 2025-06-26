783d05a57cb6f2c92d5f73d744e4c5fd994a93af
Santiago Villegas <guevara-villegas@hotmail.com>
2025-06-26 14:14:19 -0500

    [ADD]test: Update test structure module with base.

---
+++ test/test/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Test',
+    'version': '1.0',
+    'category': 'Test',
+    'sequence': 15,
+    'summary': 'Test Module',
+    'website': 'trescloud.com',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
