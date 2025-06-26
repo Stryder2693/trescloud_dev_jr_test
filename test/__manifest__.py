3b71be3405e6bdd11482b3146139db5a97df20d7
Santiago Villegas <guevara-villegas@hotmail.com>
2025-06-26 13:15:16 -0500

    [ADD]test: ADD TEST MODULE STRUCTURE.

---
+++ test/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- encoding:utf-8 -*-
+
+{
+    'name': 'Test devlorper jr trescloud',
+    'version': '1.0',
+    'category': '',
+    'description': '''
+        Este módulo tiene es para la prueba de desarrollador jr de trescloud.
+        Autores:
+            - Santiago Villegas
+    ''',
+    'author': 'TRESCLOUD CIA. LTDA.',
+    'maintainer': 'TRESCLOUD CIA. LTDA.',
+    'website': 'https://www.trescloud.com',
+    'summary': 'Test de desarrollador jr de trescloud',
+    'license': 'OEEL-1',
+    'depends': [
+    ],
+    'data': [
+        # security
+        # views
+        # wizard
+        # report
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False
+}
+
